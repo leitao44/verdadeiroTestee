e3fe45cf2362c7e509e6a776236be60be8dddbe4
Novo <Novo@ARQUI02-FLO>
2022-06-02 15:17:55 +0100

    Adicionar arquivos de projeto.

---
+++ verdadeiroTestee/WebService1.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace verdadeiroTestee
+{
+    /// <summary>
+    /// Descrição resumida de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que esse serviço da web seja chamado a partir do script, usando ASP.NET AJAX, remova os comentários da linha a seguir. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Olá, Mundo";
+        }
+    }
+}
